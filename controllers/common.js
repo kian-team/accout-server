00cf49c6ee4262a775f8b5bdfa0a2cec15745b24
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-04-30 11:27:56 +0800

    添加文件上传，初始化项目sql文件

--- controllers/common.js
+++ controllers/common.js
@@ -1,13 +1,48 @@
 const fs = require('fs');
 const { FilePath } = require('../config')
+const Tips = require('../utils/tip');
+const uploadUrl = "http://localhost:5555/files";
 const Common  = {};
-  Common.upload = async (ctx) => {
+  Common.upload = async (ctx, next) => {
   const file = ctx.request.files.file;	// 获取上传文件
-  const reader = await fs.createReadStream(FilePath);	// 创建可读流
-  const ext = file.name.split('.').pop();		// 获取上传文件扩展名
-  const upStream = await fs.createWriteStream(`${FilePath}\\${Math.random().toString()}.${ext}`);		// 创建可写流
-  await reader.pipe(upStream);	// 可读流通过管道写入可写流
-  return ctx.body = '上传成功';
+
+  if(file.name ==''){
+    ctx.body ={
+     ...Tips[1003] 
+    }
+    return await next();
+  }
+  // 读取文件流
+  const fileReader = fs.createReadStream(file.path);
+
+  const filePath = FilePath;
+  // 组装成绝对路径
+  const fileResource = filePath + `\\${file.name}`;
+
+  /*
+   使用 createWriteStream 写入数据，然后使用管道流pipe拼接
+  */
+  const writeStream = fs.createWriteStream(fileResource);
+  // 判断 /static/upload 文件夹是否存在，如果不在的话就创建一个
+  if (!fs.existsSync(filePath)) {
+    fs.mkdir(filePath, (err) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        fileReader.pipe(writeStream);
+        ctx.body = {
+          url: uploadUrl + `/${file.name}`,
+          ...Tips[0]
+        };
+      }
+    });
+  } else {
+    fileReader.pipe(writeStream);
+    ctx.body = {
+      url: uploadUrl + `/${file.name}`,
+      ...Tips[0]
+    };
+  }
 }
 
 module.exports = Common;
